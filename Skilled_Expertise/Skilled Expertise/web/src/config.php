bf65628c240bce71b7bdd5b3b510151e542e1572
b00144312 <114091004+b00144312@users.noreply.github.com>
2023-04-03 19:59:23 +0100

    Update config.php

--- Skilled_Expertise/Skilled Expertise/web/src/config.php
+++ Skilled_Expertise/Skilled Expertise/web/src/config.php
@@ -7,7 +7,7 @@
 
 $host = "localhost";
 $username = "root";
-$password = "Jtyr4544";
+$password = "Nico.270.Gherman";
 $dbname = "test"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; //will use later
 $options = array(
